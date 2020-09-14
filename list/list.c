8eea4cb6165d56e6b6c216698c1f36bf62ba2434
Peterkams <32282049+Peterkams@users.noreply.github.com>
2020-09-13 20:42:12 -0400

    Added functionality for list_alloc,list_free,list_print, list_length.

--- list/list.c
+++ list/list.c
@@ -8,22 +8,51 @@
 
 #include "list.h"
 
-list_t *list_alloc() { return NULL; }
+list_t *list_alloc() {
+  list_t *linked_list;
+  linked_list = malloc(sizeof(list_t));
+	linked_list->head = NULL;
+    return linked_list;
+}
 
 void list_free(list_t *l) {
   //struct node* temp;
-  node_t temp;
+  /*node_t temp;
   
   while(l->head != NULL)
   {
     temp = l->head;
     l->head = node_t->next;
     free(tmp);
+  }*/
+  if (!l) {
+    return;
   }
+  free (l);
 }
 
-void list_print(list_t *l) {}
-int list_length(list_t *l) { return -1; }
+void list_print(list_t *l) {
+  node_t *current_node = l->head;
+  node_t *next_node = NULL;
+  while (current_node->next != NULL)
+    {
+        printf("%d -> ", current_node->value);
+        next_node = current_node->next;
+        current_node = next_node;
+    }
+	printf("%d", current_node->value);
+	printf("\n");
+}
+
+int list_length(list_t *l) {
+    int length;
+    node_t *current_node = l->head;
+    while(current_node != NULL){
+        length++;
+        current_node = current_node->next;
+    }
+    return length; 
+}
 
 void list_add_to_back(list_t *l, elem value) {}
 void list_add_to_front(list_t *l, elem value) {}
